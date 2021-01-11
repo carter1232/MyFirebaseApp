8f4591d0ed828cce4338e2303de939bb44d6815a
Carter Maxwell <cmaxwell@imsa.edu>
2021-01-11 14:38:04 -0600

    Basic functionality comlete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kanban-fire';
+  todo = ['Buy Milk', 'Build a Kanban Board'];
+  inProgress = [];
+  done = [];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.container.id == event.previousContainer.id) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addItem(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.todo.push(input.value);
+    input.value = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatInputModule } from '@angular/material/input';
+
+//Firebase Modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC2tfL0OsMjFOVbq_8m1pfcZj3h6XhRUX0',
+  authDomain: 'personal-website-66a3a.firebaseapp.com',
+  projectId: 'personal-website-66a3a',
+  storageBucket: 'personal-website-66a3a.appspot.com',
+  messagingSenderId: '753754669976',
+  appId: '1:753754669976:web:9bc78fde724c3021a661ed',
+  measurementId: 'G-XJMG28KHDY',
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    DragDropModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
